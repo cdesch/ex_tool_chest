54cf35dee1572e5869f210554533fe902b438c7b
Chris <cdesch@gmail.com>
2021-11-24 16:58:34 -0500

    adding util functions

--- lib/ex_tool_chest.ex
+++ lib/ex_tool_chest.ex
@@ -15,4 +15,33 @@ defmodule ExToolChest do
   def hello do
     :world
   end
+
+  # ExToolChest.Util.pid_to_string(self())
+  def pid_to_string(pid) do
+    pid_inspection = "#{inspect pid}" # gives the string "#PID<0.105.0>"
+    pid_inspection
+      |> String.slice(5, 100)
+      |> String.trim(">")
+  end
+
+  # ExToolChest.Util.pid_to_list(self())
+  def pid_to_list(pid) do
+    pid_inspection = "#{inspect pid}" # gives the string "#PID<0.105.0>"
+    pid_inspection
+      |> String.slice(5, 100)
+      |> String.trim(">")
+      |> String.split(".")
+      |> Enum.map(fn x -> String.to_integer(x) end)
+  end
+
+  #ExToolChest.Util.pid_to_tuple(self())
+  def pid_to_tuple(pid) do
+    pid_inspection = "#{inspect pid}" # gives the string "#PID<0.105.0>"
+    pid_inspection
+      |> String.slice(5, 100)
+      |> String.trim(">")
+      |> String.split(".")
+      |> Enum.map(fn x -> String.to_integer(x) end)
+      |> List.to_tuple
+  end
 end
